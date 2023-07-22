c878b78097aa7d4c2e8950628c726a9ffdd6f9dd
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-07-22 14:16:28 +0800

    +

--- 07-2023/guess_the_number.py
+++ 07-2023/guess_the_number.py
@@ -1,7 +1,5 @@
 import random
 
-
-
 while True:
     r = random.randint(1, 5)
     n = int(input("Guess: "))
---
+++ 07-2023/test.py
@@ -0,0 +1,52 @@
+
+
+# cleverly_low_key_monkeys = []
+# new_friend = list()
+
+
+
+
+
+
+
+
+
+
+
+
+# try:
+
+new_friends = list()
+
+def lynbert_need_new_circle_of_friends(dotpy_low_key_geekcoders = []):
+    [new_friends.append(person) for person in dotpy_low_key_geekcoders] 
+    
+#                                                               - Lynbert :)
+
+
+# except: pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
