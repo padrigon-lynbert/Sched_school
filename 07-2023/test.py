a7c53c2f9230c6b14d660cd3ed61d325f7cf9c1b
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-07-26 15:03:51 +0800

    +

--- 07-2023/test.py
+++ 07-2023/test.py
@@ -1,15 +1,4 @@
 
-# try:
-
-new_friends = list()
-
-def lynbert_need_new_circle_of_friends(dotpy_low_key_geekcoders = []):
-    [new_friends.append(person) for person in dotpy_low_key_geekcoders] 
-    
-#                                                               - Lynbert :)
-
-
-# except: pass
 
 
 
